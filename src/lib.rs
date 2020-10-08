2cc57727da7afff5b7765daf788674e6e9fd18d5
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2020-10-08 22:18:40 +0800

    Implement SHA1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod md5;
+mod sha1;
 
 pub use md5::md5;
+pub use sha1::sha1;
---
+++ src/sha1.rs
@@ -0,0 +1,101 @@
+pub fn sha1(message: &[u8]) -> String {
+    let mut message = message
+        .iter()
+        .map(|byte| format!("{:0>8b}", byte))
+        .collect::<String>();
+    let message_len = format!("{:0>64b}", message.len() as u64);
+    message.push('1');
+
+    while (message.len() + 64) % 512 != 0 {
+        message.push('0');
+    }
+
+    message.push_str(&message_len);
+    let mut h0: u32 = 0x67452301;
+    let mut h1: u32 = 0xefcdab89;
+    let mut h2: u32 = 0x98badcfe;
+    let mut h3: u32 = 0x10325476;
+    let mut h4: u32 = 0xc3d2e1f0;
+
+    for chunk_index in 0..message.len() / 512 {
+        let chunk = &message[chunk_index * 512..chunk_index * 512 + 512];
+        let mut w = (0..16)
+            .map(|i| {
+                u32::from_str_radix(&chunk[i * 32..i * 32 + 32], 2).unwrap()
+            })
+            .collect::<Vec<u32>>();
+
+        for i in 16..80 {
+            w.push(
+                (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]).rotate_left(1),
+            );
+        }
+
+        let (mut a, mut b, mut c, mut d, mut e) = (h0, h1, h2, h3, h4);
+
+        for i in 0..80 {
+            let (f, k): (u32, u32);
+
+            if i < 20 {
+                f = (b & c) | (!b & d);
+                k = 0x5a827999;
+            } else if i >= 20 && i < 40 {
+                f = b ^ c ^ d;
+                k = 0x6ed9eba1;
+            } else if i >= 40 && i < 60 {
+                f = (b & c) | (b & d) | (c & d);
+                k = 0x8f1bbcdc;
+            } else {
+                f = b ^ c ^ d;
+                k = 0xca62c1d6;
+            }
+
+            e = e.wrapping_add(f);
+            e = e.wrapping_add(a.rotate_left(5));
+            e = e.wrapping_add(w[i]);
+            e = e.wrapping_add(k);
+            let temp = e;
+            e = d;
+            d = c;
+            c = b.rotate_left(30);
+            b = a;
+            a = temp;
+        }
+
+        h0 = h0.wrapping_add(a);
+        h1 = h1.wrapping_add(b);
+        h2 = h2.wrapping_add(c);
+        h3 = h3.wrapping_add(d);
+        h4 = h4.wrapping_add(e);
+    }
+
+    [
+        format!("{:0>8x}", h0),
+        format!("{:0>8x}", h1),
+        format!("{:0>8x}", h2),
+        format!("{:0>8x}", h3),
+        format!("{:0>8x}", h4),
+    ]
+    .join("")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sha1_test() {
+        assert_eq!(
+            "da39a3ee5e6b4b0d3255bfef95601890afd80709".to_string(),
+            sha1("".as_bytes()),
+        );
+        assert_eq!(
+            "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12".to_string(),
+            sha1("The quick brown fox jumps over the lazy dog".as_bytes()),
+        );
+        assert_eq!(
+            "408d94384216f890ff7a0c3528e8bed1e0b01621".to_string(),
+            sha1("The quick brown fox jumps over the lazy dog.".as_bytes()),
+        );
+    }
+}
