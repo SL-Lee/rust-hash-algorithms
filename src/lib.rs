ca181ac59ef844f3ce55b7d879122d9262125f96
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2020-10-10 16:47:13 +0800

    Implement a wrapper around SHA-512

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,6 @@ pub use sha1::SHA1;
 pub use sha224::SHA224;
 pub use sha256::SHA256;
 pub use sha384::SHA384;
-pub use sha512::sha512;
+pub use sha512::SHA512;
 pub use sha512_224::sha512_224;
 pub use sha512_256::sha512_256;
